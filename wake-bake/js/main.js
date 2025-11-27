9d788d5aac6932d8ebec44a2019b6725dd9a6806
Tsarkov-web <tsarkov0405@gmail.com>
2025-11-27 16:17:17 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -6,17 +6,36 @@
         const burgerNavLink = e.target.closest('.nav__link')
 
 
-        
+
 
         if (!burgerIcon && !burgerNavLink) return
 
-        if (document.documentElement.clientWidth > 900) return 
+        if (document.documentElement.clientWidth > 900) return
 
         if (!document.body.classList.contains('body__opened_menu')) {
             document.body.classList.add('body__opened_menu')
         } else {
             document.body.classList.remove('body__opened_menu')
         }
+
+    }
+})()
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const aboutImgButton = document.querySelector('.about__img-button');
+    const modalCancel = document.querySelector('.modal__cancel');
+    const body = document.body;
+
+    if (aboutImgButton) {
+        aboutImgButton.addEventListener('click', function () {
+            body.classList.add('body__opened-modal');
+        });
     }
 
-})()
+    if (modalCancel) {
+        modalCancel.addEventListener('click', function () {
+            body.classList.remove('body__opened-modal');
+        });
+    }
+});
